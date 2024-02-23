8743cc9fae8a2af576a5787a45c35d8035892062
Nicolas <rojonicolasdev@gmail.com>
2024-02-23 16:51:33 -0300

    [ADDED] Init repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+interface Country {
+  name: string;
+}
+
+const countriesData: Country[] = [
+  { name: "India" },
+  { name: "USA" },
+  { name: "France" },
+];
 
 function App() {
+  const [selectedCountries, setSelectedCountries] = useState<Set<string>>(
+    new Set()
+  );
+  const allCountries = countriesData.map((country) => country.name);
+
+  const handleCountryClick = (countryName: string) => {
+    const updatedSelectedCountries = new Set(selectedCountries);
+
+    updatedSelectedCountries.has(countryName)
+      ? updatedSelectedCountries.delete(countryName)
+      : updatedSelectedCountries.add(countryName);
+
+    setSelectedCountries(updatedSelectedCountries);
+  };
+
+  const handleSelectAll = () =>
+    setSelectedCountries(
+      selectedCountries.size === allCountries.length
+        ? new Set()
+        : new Set(allCountries)
+    );
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCountries.size === allCountries.length}
+            onChange={handleSelectAll}
+          />
+          Select All
+        </label>
+        {countriesData.map((country, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="checkbox"
+                checked={selectedCountries.has(country.name)}
+                onChange={() => handleCountryClick(country.name)}
+              />
+              {country.name}
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
